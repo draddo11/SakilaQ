eedb726f0333f2d0a5566d26607992068dc9bb80
Onesimus Addo Appiah <draddo11@gmail.com>
2024-03-31 22:50:16 +0000

    added paths to my FilmResource.java

--- src/main/java/FilmResource.java
+++ src/main/java/FilmResource.java
@@ -1,2 +1,16 @@
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.awt.*;
+
+@Path("/")
 public class FilmResource {
+    @GET
+    @Path("/helloWorld")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello(){
+        return "Hello World";
+    }
 }
